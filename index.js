b3a7659b217b05096e73054b6130f49cc85d44b0
Krishna Pandit <krishnakumarji100@gmail.com>
2025-11-06 11:50:14 +0530

    application scale with adding multiple initial state and multiple reducer

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // const {createStore } = require('redux');
 const redux = require('redux');
 const createStore = redux.createStore;
+const combineReducers = redux.combineReducers
 
 console.log("Hello, World!");
 
@@ -24,18 +25,27 @@ function buyIceCream(){
 }
 
 // Initial State
-const initialState = {
-    numOfCakes:10,
+const initialCakeState = {
+    numOfCakes:10
+}
+
+const initialIceCreamState = {
     numOfIceCreams:20
 }
 
 // Reducer
-const reducer = (state = initialState, action)=>{
+const cakeReducer = (state = initialCakeState, action)=>{
     switch(action.type){
         case BUY_CAKE : return{
             ...state,
             numOfCakes: state.numOfCakes - 1
         }
+        default: return state;
+    }
+}
+
+const iceCreamReducer = (state = initialIceCreamState, action)=>{
+    switch(action.type){
         case BUY_ICECREAM: return{
             ...state,
             numOfIceCreams: state.numOfIceCreams-1
@@ -45,7 +55,11 @@ const reducer = (state = initialState, action)=>{
 }
 
 // Redux Store
-const store = createStore(reducer);
+const rootReducer = combineReducers({
+    cake:cakeReducer,
+    iceCream:iceCreamReducer
+})
+const store = createStore(rootReducer);
 console.log('Initial State', store.getState());
 const unsubscribe = store.subscribe(()=> console.log('Updated State', store.getState()));
 store.dispatch(buyCake());
