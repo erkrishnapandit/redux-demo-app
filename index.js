1e88b190fdc8dbc9e1ce0d91a0ed303dd95715e5
Krishna Pandit <krishnakumarji100@gmail.com>
2025-11-06 11:11:37 +0530

    Basic Redux Structure is Created

---
+++ index.js
@@ -0,0 +1,56 @@
+// const {createStore } = require('redux');
+const redux = require('redux');
+const createStore = redux.createStore;
+
+console.log("Hello, World!");
+
+// Action Type
+const BUY_CAKE = "BUY_CAKE";
+const BUY_ICECREAM = 'BUY_ICECREAM';
+
+// Action Creator
+function buyCake(){
+    return{
+        type:BUY_CAKE,
+        info:"First Redux Action"
+    }
+}
+
+function buyIceCream(){
+    return{
+        type:BUY_ICECREAM,
+        info:"Second Redux Action"
+    }
+}
+
+// Initial State
+const initialState = {
+    numOfCakes:10,
+    numOfIceCreams:20
+}
+
+// Reducer
+const reducer = (state = initialState, action)=>{
+    switch(action.type){
+        case BUY_CAKE : return{
+            ...state,
+            numOfCakes: state.numOfCakes - 1
+        }
+        case BUY_ICECREAM: return{
+            ...state,
+            numOfIceCreams: state.numOfIceCreams-1
+        }
+        default: return state;
+    }
+}
+
+// Redux Store
+const store = createStore(reducer);
+console.log('Initial State', store.getState());
+const unsubscribe = store.subscribe(()=> console.log('Updated State', store.getState()));
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+store.dispatch(buyIceCream());
+store.dispatch(buyIceCream());
+unsubscribe();
